ab708b37a5da8da5a6aa0c116f426b17c453c33c
abhishekzambre <abhishek.zambre@gmail.com>
2016-09-02 23:53:15 -0700

    Light Sensor Demo

---
+++ LightSensorDemo/app/src/main/java/me/abhishekz/lightsensordemo/MainActivity.java
@@ -0,0 +1,62 @@
+package me.abhishekz.lightsensordemo;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView textLIGHT_available, textLIGHT_reading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textLIGHT_available
+                = (TextView)findViewById(R.id.LIGHT_available);
+        textLIGHT_reading
+                = (TextView)findViewById(R.id.LIGHT_reading);
+
+        SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(LightSensor != null){
+            textLIGHT_available.setText("Sensor.TYPE_LIGHT Available");
+            mySensorManager.registerListener(
+                    LightSensorListener,
+                    LightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        }else{
+            textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
+        }
+
+    }
+
+    private final SensorEventListener LightSensorListener
+            = new SensorEventListener(){
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+                textLIGHT_reading.setText("Light : "+ String.valueOf(event.values[0]));
+            }
+        }
+
+    };
+
+}
